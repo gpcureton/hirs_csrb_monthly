a69f9dfd29754aed0f0d5e17c27dabef2aade74a
Geoff Cureton <geoff.cureton@ssec.wisc.edu>
2018-05-17 05:27:16 +0000

    Updated cluster submission script.

--- submit_hirs_csrb_monthly.py
+++ submit_hirs_csrb_monthly.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
 # encoding: utf-8
+"""
+
+Purpose: Run the hirs_csrb_monthly package
+
+Copyright (c) 2015 University of Wisconsin Regents.
+Licensed under GNU GPLv3.
+"""
+
 
 import sys
 import traceback
@@ -11,12 +19,9 @@ from time import sleep
 from flo.ui import safe_submit_order
 from timeutil import TimeInterval, datetime, timedelta
 
-#from flo.sw.hirs_csrb_daily import HIRS_CSRB_DAILY
-#from flo.sw.hirs_csrb_monthly import HIRS_CSRB_MONTHLY
-
 import flo.sw.hirs_csrb_daily as hirs_csrb_daily
 import flo.sw.hirs_csrb_monthly as hirs_csrb_monthly
-from flo.sw.hirs.utils import setup_logging
+from flo.sw.hirs2nc.utils import setup_logging
 
 # every module should have a LOG object
 LOG = logging.getLogger(__name__)
@@ -24,9 +29,10 @@ LOG = logging.getLogger(__name__)
 setup_logging(2)
 
 # General information
-hirs_version = 'v20151014'
-collo_version = 'v20151014'
-csrb_version = 'v20150915'
+hirs2nc_delivery_id = '20180410-1'
+hirs_avhrr_delivery_id = '20180505-1'
+hirs_csrb_daily_delivery_id = '20180511-1'
+hirs_csrb_monthly_delivery_id  = '20180516-1'
 wedge = timedelta(seconds=1.)
 day = timedelta(days=1.)
 
@@ -34,31 +40,24 @@ day = timedelta(days=1.)
 
 #satellite = 'noaa-19'
 #granule = datetime(2015, 4, 17, 0, 20)
-#intervals = [TimeInterval(granule, granule + wedge - wedge)]
-#intervals = []
-#years = 2009
-#intervals += [TimeInterval(datetime(years,month,1), datetime(years,month,calendar.monthrange(years,month)[1])+day-wedge) for month in range(4,13) ]
-#for years in range(2010, 2018):
-    #intervals += [TimeInterval(datetime(years,month,1), datetime(years,month,calendar.monthrange(years,month)[1])+day-wedge) for month in range(1,13) ]
-#years = 2018
-#intervals += [TimeInterval(datetime(years,month,1), datetime(years,month,calendar.monthrange(years,month)[1])+day-wedge) for month in range(1,2) ]
+#intervals = [TimeInterval(granule, granule + day - wedge)]
+# NSS.GHRR.NP.D09108.S2301.E0050.B0100809.GC.level2.hdf --> NSS.GHRR.NP.D17365.S2238.E2359.B4585757.GC.level2.hdf
+# datetime(2009, 4, 18, 0, 0) --> datetime(2017, 12, 31, 0, 0)
+#intervals = [TimeInterval(datetime(years,1,1), datetime(years+1,1,1)-wedge) for years in range(2009, 2018) ]
 
 satellite = 'metop-b'
-intervals = []
-years = 2017
-#intervals += [TimeInterval(datetime(years, 1, 1), datetime(years, 2, 1)-wedge)]
-intervals += [TimeInterval(datetime(years, 1, 1), datetime(years+1, 1, 1)-wedge)]
-#intervals += [TimeInterval(datetime(years,month,1), datetime(years,month,calendar.monthrange(years,month)[1])+day-wedge) for month in range(1,13) ]
-#for years in range(2013,2018):
-    #intervals += [TimeInterval(datetime(years,month,1), datetime(years,month,calendar.monthrange(years,month)[1])+day-wedge) for month in range(1,13) ]
-
-# Instantiate the computation
-hirs_csrb_daily_comp = hirs_csrb_daily.HIRS_CSRB_DAILY()
-comp = hirs_csrb_monthly.HIRS_CSRB_MONTHLY()
+#intervals = [TimeInterval(datetime(2015,1,1), datetime(2015,2,1)-wedge)]
+# NSS.GHRR.M1.D13140.S0029.E0127.B0347172.SV.level2.hdf --> NSS.GHRR.M1.D15365.S2307.E0004.B1705253.SV.level2.hdf
+# datetime(2013, 5, 20, 0, 0) --> datetime(2017, 12, 31, 0, 0)
+intervals = [TimeInterval(datetime(years,1,1), datetime(years+1,1,1)-wedge) for years in range(2013, 2018) ]
 
 satellite_choices = ['noaa-06', 'noaa-07', 'noaa-08', 'noaa-09', 'noaa-10', 'noaa-11',
                     'noaa-12', 'noaa-14', 'noaa-15', 'noaa-16', 'noaa-17', 'noaa-18',
                     'noaa-19', 'metop-a', 'metop-b']
+
+# Instantiate the computation
+hirs_csrb_daily_comp = hirs_csrb_daily.HIRS_CSRB_DAILY()
+comp = hirs_csrb_monthly.HIRS_CSRB_MONTHLY()
 
 LOG.info("Submitting intervals...")
 
@@ -74,7 +73,8 @@ try:
     for interval in intervals:
         LOG.info("Submitting interval {} -> {}".format(interval.left, interval.right))
 
-        contexts = comp.find_contexts(interval, satellite, hirs_version, collo_version, csrb_version)
+        contexts = comp.find_contexts(interval, satellite, hirs2nc_delivery_id, hirs_avhrr_delivery_id, hirs_csrb_daily_delivery_id,
+                          hirs_csrb_monthly_delivery_id)
 
         LOG.info("Opening log file {}".format(log_name))
         file_obj = open(log_name,'a')
@@ -83,8 +83,8 @@ try:
         contexts.sort()
 
         if contexts != []:
-            for context in contexts:
-                LOG.info(context)
+            #for context in contexts:
+                #LOG.info(context)
 
             LOG.info("\tFirst context: {}".format(contexts[0]))
             LOG.info("\tLast context:  {}".format(contexts[-1]))
